8582b6f2d7d8634f5cd2d6d2aaf766e7c3a91c5d
Yevhen Perediela <peredelaevgen23@gmail.com>
2025-04-03 09:26:07 +0200

    kamera

--- kamera2.py
+++ kamera2.py
@@ -1,9 +1,25 @@
+from picamera2 import Picamera2
 import cv2
 
-for i in range(5):
-    cap = cv2.VideoCapture(i)
-    if cap.isOpened():
-        print(f"Kamera znaleziona na indexie: {i}")
-        cap.release()
-    else:
-        print(f"Brak kamery na indexie: {i}")
+# Inicjalizacja kamery
+picam2 = Picamera2()
+
+# Konfiguracja rozdzielczości i formatu obrazu
+picam2.preview_configuration.main.size = (640, 480)
+picam2.preview_configuration.main.format = "BGR888"
+picam2.configure("preview")
+
+# Start kamery
+picam2.start()
+
+# Pętla wyświetlająca obraz
+while True:
+    frame = picam2.capture_array()
+    cv2.imshow("Podgląd z kamery Raspberry Pi", frame)
+
+    # Naciśnij 'q', żeby zamknąć podgląd
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Zwolnienie zasobów
+cv2.destroyAllWindows()
